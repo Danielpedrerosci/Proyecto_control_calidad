08047098c7d26bdd587396ca29d3b3022b351f42
Danielpedrerosci <d-cifuentes@hotmail.com>
2025-07-22 23:54:30 -0500

    Arreglo rutas main.py

--- deployment/main.py
+++ deployment/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf 
 import numpy as np 
 def model_prediction(test_image):
-    model = tf.keras.models.load_model("/Users/danielpedreroscifuentes/Downloads/fruits/modelo_entrenado.h5")
+    model = tf.keras.models.load_model("trained_best_model.keras")
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
@@ -18,7 +18,7 @@ if (app_mode=="Inicio"):
     st.markdown(
     "<h2 style='text-align: center;'>Identificación del estado de productos de exportación perecederos para mejorar el proceso de control de calidad: Bananos, Manzanas y Naranjas</h2>",
     unsafe_allow_html=True)
-    image_path = "/Users/danielpedreroscifuentes/Downloads/fruits/img_01.jpg"
+    image_path = "img_01.jpg"
     st.image(image_path)
 
 #About project
@@ -31,7 +31,7 @@ elif(app_mode=="Información general"):
     st.text("En lo que se lleva del 2025 las exportaciones del grupo de productos Agropecuarios,\nalimentos y bebidas ha tenido una contribución de 11 Millones de\ndólares habiendo crecido un 36.9% versus el inicio del año según cifras del DANE\ny su tendencia es positiva. Dado que la demanda de este tipo de productos crece,\nes crucial un buen control de calidad y contar con herramientas tecnológicas que permitan una mayor efectividad y precisión a la hora de escoger estos productos.\nEste rubro será vital para la economía colombiana y para todas las empresas que\nestán en busca de nuevos clientes internacionales y genera confianza en la calidad de sus productos.")
     st.subheader("Expectativas")
     st.text("Mejorar la efectividad del proceso de selección de frutas, minimizando el riesgo de\nreportar frutas en mal estado en pedidos de exportación y evitar\npérdidas monetarias y de reputación a la compañía.")
-    image_path = "/Users/danielpedreroscifuentes/Downloads/fruits/img_02.jpg"
+    image_path = "img_02.jpg"
     st.image(image_path, caption="Exportaciones 2025", use_column_width=True)
 #Identificación del producto
 elif(app_mode=="Identificación del producto"):
